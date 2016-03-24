b820700f0015cb3548d3a097742a07e3e87c7204
Jeremy Hughes <jedahu@gmail.com>
2016-03-24 22:24:23 +1300

    Change pass bindings

--- layers/+tools/pass/packages.el
+++ layers/+tools/pass/packages.el
@@ -6,8 +6,16 @@
     :commands (pass)
     :config
     (progn
-      (evilified-state-evilify-map pass-mode-map
-        :mode pass-
-        :bindings
+      (evilified-state-evilify pass-mode pass-mode-map
         (kbd "RET") 'pass-view
+        "y" 'pass-copy
+        "j" 'pass-next-entry
+        "k" 'pass-prev-entry
+        "gj" 'pass-next-directory
+        "gk" 'pass-prev-directory
+        "d" 'pass-kill
+        "i" 'pass-insert
+        "c" 'pass-rename
+        "q" 'pass-quit
+        "r" 'pass-update-buffer
         ))))
