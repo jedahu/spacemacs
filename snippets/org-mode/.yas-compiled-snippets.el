2a8c7004ea251aa56a91181333295c89d5f7b904
Jeremy Hughes <jedahu@gmail.com>
2017-01-23 16:37:42 +1300

    add snippets

---
+++ snippets/org-mode/.yas-compiled-snippets.el
@@ -0,0 +1,9 @@
+;;; Compiled snippets and support files for `org-mode'
+;;; Snippet definitions:
+;;;
+(yas-define-snippets 'org-mode
+                     '(("ss" "src_scala{$1" "org src_scala" nil nil nil "/home/jdh/.emacs.d/snippets/org-mode/org-src-scala" nil nil)
+                       ("src" "src_$1{$2" "org src inline" nil nil nil "/home/jdh/.emacs.d/snippets/org-mode/org-src-inline" nil nil)))
+
+
+;;; Do not edit! File generated at Mon Aug 22 11:30:40 2016
