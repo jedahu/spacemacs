eb646bf2a1b9a8a2fd69d09ea7dda99b4d4d29ca
Jeremy Hughes <jeremy.hughes@arlo.co>
2017-04-10 16:29:23 +1200

    update packages

--- elpa/company-flow-20170208.756/company-flow-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "company-flow" "20170208.756" "Flow backend for company-mode" '((company "0.8.0") (dash "2.13.0")) :url "https://github.com/aaronjensen/company-flow")
--- elpa/restclient-helm-20160407.249/restclient-helm-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "restclient-helm" "20160407.249" "helm interface for restclient.el" '((restclient "0") (helm "1.9.4")) :keywords '("http" "helm"))
