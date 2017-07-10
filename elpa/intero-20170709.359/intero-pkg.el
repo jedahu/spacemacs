f9ed9831b475a26ecb7f7f9a1fd12c4ec1feffe5
Jeremy Hughes <jedahu@gmail.com>
2017-07-11 11:56:40 +1200

    add new packages

---
+++ elpa/intero-20170709.359/intero-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "intero" "20170709.359" "Complete development mode for Haskell" '((flycheck "0.25") (company "0.8") (emacs "24.4") (haskell-mode "13.0")) :commit "7e72aaadadf6f03f1f76fb1f34699265f70c673a" :url "https://github.com/commercialhaskell/intero" :keywords '("haskell" "tools"))
---
+++ elpa/mocha-20170710.816/mocha-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "mocha" "20170710.816" "Run Mocha or Jasmine tests" '((js2-mode "20150909") (f "0.18")) :commit "8e585b6dd21e4f387e3eb1b1af742558d73980c8" :url "http://github.com/scottaj/mocha.el" :keywords '("javascript" "mocha" "jasmine"))
