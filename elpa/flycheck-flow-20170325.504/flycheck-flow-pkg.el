47f3272504003c5aa4161a5f91a8e627e49d9018
Jeremy Hughes <jedahu@gmail.com>
2017-07-11 11:56:00 +1200

    update packages

--- elpa/flycheck-flow-20170325.504/flycheck-flow-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "flycheck-flow" "20170325.504" "Support Flow in flycheck" '((flycheck "0.18") (json "1.4")))
--- elpa/outorg-20170402.1415/outorg-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "outorg" "20170402.1415" "Org-style comment editing" '((emacs "24.4")) :url "https://github.com/alphapapa/outorg")
